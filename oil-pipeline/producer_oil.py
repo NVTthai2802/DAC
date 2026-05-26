f928209f21094aa5ce3d8fdb08e912b18e7dbb9f
NVTThai2802 <nvthai2802@gmail.com>
2026-05-26 09:52:59 +0700

    feat: create forex dashboard

--- oil-pipeline/producer_oil.py
+++ oil-pipeline/producer_oil.py
@@ -6,7 +6,7 @@ import yfinance as yf
 
 # Cấu hình Kafka
 producer = KafkaProducer(
-    bootstrap_servers=['localhost:9092'],
+    bootstrap_servers=['localhost:29092'],
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
